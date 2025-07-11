27fb16fd2e185edcda91d756abb1bf4c2af54da1
mrunalawate <mrunalawate0tv@gmail.com>
2025-07-11 23:58:09 +0530

    Initial commit of StockPulse project

--- backend/utils/whatsappNotifier.js
+++ backend/utils/whatsappNotifier.js
@@ -1,17 +1,17 @@
 const twilio = require("twilio");
 
-const accountSid = "AC4cb39edce5638aa37adcee4cf9b20595";
-const authToken = "9fed8b3f78aa3999bc3651e143f0a9c5";
+const accountSid = "process.env.TWILIO_ACCOUNT_SID";
+const authToken = "process.env.TWILIO_ACCOUNT_SID";
 const client = twilio(accountSid, authToken);
 
-const TO_NUMBER = "whatsapp:+918591061115"; // Your verified WhatsApp number
+const TO_NUMBER = "whatsapp:+9100000000"; // Your verified WhatsApp number
 
 const sendWhatsAppAlert = async (productName, quantity, forecast) => {
   const message = `🚨 *Stock Alert for ${productName}*:\nOnly ${quantity} left in stock.\nForecast (next 7 days): ${forecast}\n📦 Restock suggested.`;
 
   try {
     await client.messages.create({
-      from: "whatsapp:+14155238886", // Twilio sandbox WhatsApp number
+      from: "whatsapp:+1000000000", // Twilio sandbox WhatsApp number
       to: TO_NUMBER,
       body: message,
     });
